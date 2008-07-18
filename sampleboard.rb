6416c1a98f92145a8033fc46a78de9ff735ff8f1
Erik Kastner <kastner@gmail.com>
2008-07-18 16:12:39 -0400

    works now

--- sampleboard.rb
+++ sampleboard.rb
@@ -20,17 +20,13 @@ open('http://www.freesound.org/packsViewSingle.php?id=1454').read.scan(/http:\/\
 end
 
 puts "Done. Now hit number 0-9 to play sounds. (ctrl-c or ESC to end)"
-
-OSX.ruby_thread_switcher_start(0.001, 0.1)
-Thread.start { NSRunLoop.currentRunLoop.run }
+ }
 
 while(1)
   c = read_char
   break if c == 27 || c == 3
   c = c.chr.to_i
   if c && mp3s[c]
-    Thread.start do
-      mp3s[c].play
-    end.join
+    Thread.new { mp3s[c].stop; mp3s[c].play }
   end
 end
