96b34a3ae6bbac290d7749a406371c4ad997ec30
Erik Kastner <kastner@gmail.com>
2008-07-18 15:46:36 -0400

    using threads like sndplay2.rb

--- sampleboard.rb
+++ sampleboard.rb
@@ -13,24 +13,24 @@ end
 
 puts "Fetching MP3s..."
 mp3s = []
-# if working with local files: Dir["/tmp/mp3/*.mp3"].each_with_index do |mp3,i|
 open('http://www.freesound.org/packsViewSingle.php?id=1454').read.scan(/http:\/\/[^"]+?\.mp3/).each_with_index do |mp3, i|
   break if i > 9
   puts "Loading #{mp3}"
   mp3s << NSSound.alloc.initWithContentsOfURL_byReference(NSURL.alloc.initWithString(mp3), 1)
-  # if working with local files: # mp3s << NSSound.alloc.initWithContentsOfFile_byReference(mp3, 0)
 end
 
 puts "Done. Now hit number 0-9 to play sounds. (ctrl-c or ESC to end)"
+
+OSX.ruby_thread_switcher_start(0.001, 0.1)
+Thread.start { NSRunLoop.currentRunLoop.run }
 
 while(1)
   c = read_char
   break if c == 27 || c == 3
   c = c.chr.to_i
   if c && mp3s[c]
-    # this is broken, doesn't release the memory, just gets bigger and bigger
-    sound = mp3s[c].copy
-    sound.autorelease
-    sound.play
+    Thread.start do
+      mp3s[c].play
+    end.join
   end
 end
